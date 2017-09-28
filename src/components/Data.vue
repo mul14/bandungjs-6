443ad7329fe5b9e98748032c479e674fd867287f
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-09-28 12:16:58 +0700

    More update

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 style="font-weight: lighter; color: #47b784;">show data</h1>
+    <h1 style="font-weight: lighter; color: #47b784;">$data</h1>
     <hr>
 
     <div class="row">
@@ -8,7 +8,7 @@
       <div class="col-6"><pre>{{ $data }}</pre></div>
       <div class="col-6">
         <ul>
-          <li v-for="item in items">{{  item.name }}</li>
+          <li v-for="(item, index) in items" :key="index">{{  item.name }}</li>
         </ul>
       </div>
     </div>
