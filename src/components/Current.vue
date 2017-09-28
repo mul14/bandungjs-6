9d5cc9503e394bf477fb382ecfe25c5db3f2c636
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-09-28 17:05:22 +0700

    Adjust image src

--- src/components/Current.vue
+++ src/components/Current.vue
@@ -28,13 +28,13 @@
 
     <div style="padding: 600px 0;">
       <h2 class="mt-5" style="font-weight: lighter; color: #47b784;">google trends</h2>
-      <img src="/static/trends.png" alt="" class="img-fluid">
+      <img src="/bandungjs-6/static/trends.png" alt="" class="img-fluid">
     </div>
 
     <div style="padding: 600px 0;">
       <h2 class="mb-5" style="font-weight: lighter; color: #47b784;">github</h2>
-      <div><img src="/static/github-stars.png" alt="" class="img-fluid"></div>
-      <div><img src="/static/github-trends.png" alt="" class="img-fluid"></div>
+      <div><img src="/bandungjs-6/static/github-stars.png" alt="" class="img-fluid"></div>
+      <div><img src="/bandungjs-6/static/github-trends.png" alt="" class="img-fluid"></div>
     </div>
 
     <div style="padding: 600px 0;">
@@ -42,7 +42,7 @@
       <h2 class="mt-5" style="font-weight: lighter; color: #47b784;">npm</h2>
       <div class="row">
         <div class="col-4"></div>
-        <div class="col-4 mb-5"><img src="/static/npm.png" alt="" class="img-fluid"></div>
+        <div class="col-4 mb-5"><img src="/bandungjs-6/static/npm.png" alt="" class="img-fluid"></div>
         <div class="col-4"></div>
         <div class="col-12">
           <h2 class="text-center">~800K / month</h2>
@@ -54,58 +54,58 @@
     <div style="padding: 600px 0;">
       <h2 class="mt-5" style="font-weight: lighter; color: #47b784;">Vue Chrome Developer Tools</h2>
       <div class="text-center">
-        <div><img src="/static/vue-devtools.png" alt="" class="img-fluid"></div>
+        <div><img src="/bandungjs-6/static/vue-devtools.png" alt="" class="img-fluid"></div>
         <h2>~273K Active Users</h2>
       </div>
     </div>
 
     <div style="padding: 600px 0;">
       <h2 class="mt-5" style="font-weight: lighter; color: #47b784;">google i/o 2017</h2>
-      <div><img src="/static/google-io.jpg" alt="" class="img-fluid"></div>
+      <div><img src="/bandungjs-6/static/google-io.jpg" alt="" class="img-fluid"></div>
     </div>
 
     <div style="padding: 600px 0;">
       <h2 class="mt-5" style="font-weight: lighter; color: #47b784;">weex</h2>
-      <div><img src="/static/weex.png" alt="" class="img-fluid"></div>
+      <div><img src="/bandungjs-6/static/weex.png" alt="" class="img-fluid"></div>
     </div>
 
     <div style="padding: 600px 0;">
       <h2 class="mt-5" style="font-weight: lighter; color: #47b784;">who use vue?</h2>
       <div class="row">
         <div class="col-4 p-5">
-          <img src="/static/adobe.png" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/adobe.png" alt="" class="img-fluid">
         </div>
         <div class="col-4 p-5">
-          <img src="/static/alibaba.png" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/alibaba.png" alt="" class="img-fluid">
         </div>
         <div class="col-4 p-5">
-          <img src="/static/baidu.png" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/baidu.png" alt="" class="img-fluid">
         </div>
         <div class="col-4 p-5">
-          <img src="/static/behance.svg" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/behance.svg" alt="" class="img-fluid">
         </div>
         <div class="col-4 p-5">
-          <img src="/static/blibli.png" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/blibli.png" alt="" class="img-fluid">
         </div>
         <div class="col-4 p-5">
-          <img src="/static/codeship.png" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/codeship.png" alt="" class="img-fluid">
         </div>
         <div class="col-4 p-5">
-          <img src="/static/dji.png" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/dji.png" alt="" class="img-fluid">
         </div>
         <div class="col-4 p-5">
           <a href="https://newsfeed.fb.com/" target="_blank">
-            <img src="/static/facebook.svg" alt="" class="img-fluid">
+            <img src="/bandungjs-6/static/facebook.svg" alt="" class="img-fluid">
           </a>
         </div>
         <div class="col-4 p-5">
-          <img src="/static/gitlab.svg" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/gitlab.svg" alt="" class="img-fluid">
         </div>
         <div class="col-4 p-5">
-          <img src="/static/jakmall.png" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/jakmall.png" alt="" class="img-fluid">
         </div>
         <div class="col-4 p-5">
-          <img src="/static/nbc.png" alt="" class="img-fluid">
+          <img src="/bandungjs-6/static/nbc.png" alt="" class="img-fluid">
         </div>
       </div>
     </div>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <img src="/static/cover.jpg" alt="" class="img-fluid">
+    <img src="/bandungjs-6/static/cover.jpg" alt="" class="img-fluid">
     <h1 class="mt-5 text-muted" style="font-weight: lighter;">BandungJS #6 - Introduction to the Great Vue.js</h1>
     <div class="lead mt-4">By: Mulia Arifandi Nasution</div>
     <div class="lead text-muted">mulia@nasution.id</div>
