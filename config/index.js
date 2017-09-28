53d7056b7e0516d9a564c03b4f5bd872997ac888
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-09-28 17:32:53 +0700

    Fiuuhh

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'bandungjs-6/static/',
-    assetsPublicPath: '/bandungjs-6/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
