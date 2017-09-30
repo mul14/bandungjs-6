549b6388b8403061a8711f57d7a94940ad6e77bb
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-09-30 11:56:16 +0700

    Update Single File Component

--- src/components/Single.vue
+++ src/components/Single.vue
@@ -2,20 +2,15 @@
   <div>
     <h1 style="font-weight: lighter; color: #47b784;">single file component</h1>
     <hr>
-    <pre v-pre class="lead">
 
-      // ComponentName.vue
 
-      &lt;template&gt;
+    <div class="my-5">
+      <img src="https://vuejs.org/images/vue-component.png" alt="" class="img-fluid">
+    </div>
 
-          &lt;div&gt;&lt;/div&gt;
+    <div class="my-5">
+      <img src="https://vuejs.org/images/vue-component-with-preprocessors.png" alt="" class="img-fluid">
+    </div>
 
-      &lt;/template&gt;
-
-      &lt;script&gt;&lt;/script&gt;
-
-      &lt;style&gt;&lt;/style&gt;
-
-    </pre>
   </div>
 </template>
